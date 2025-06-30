9137c5a085a86f8b22d0f0ca02216bee85a08429
mihirkasodariya <mihirkasodariya21@gmail.com>
2025-06-30 17:20:13 +0530

    - add ads.1.1

--- src/Components/AdSenseAd.jsx
+++ src/Components/AdSenseAd.jsx
@@ -1,34 +1,22 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 const AdSenseAd = () => {
-  const adRef = useRef(null);
-
   useEffect(() => {
-    const observer = new ResizeObserver(([entry]) => {
-      console.log('window.adsbygoogle',window.adsbygoogle)
-      if (entry.contentRect.width > 0) {
-        try {
-          (window.adsbygoogle = window.adsbygoogle || []).push({});
-          observer.disconnect();
-        } catch (e) {
-          console.error("AdSense push error:", e);
-        }
-      }
-    });
-
-    if (adRef.current) observer.observe(adRef.current);
-    return () => observer.disconnect();
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error("AdSense error:", e);
+    }
   }, []);
 
   return (
     <ins
       className="adsbygoogle"
-      // style={{ display: "block", width: "100%", height: "100px" }}
-      data-ad-client="ca-google"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-xxxxxxxxxxxxxxxx"
       data-ad-slot="1234567890"
       data-ad-format="auto"
       data-full-width-responsive="true"
-      ref={adRef}
     ></ins>
   );
 };
